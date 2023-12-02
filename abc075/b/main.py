898162bcea2e2ab64ff86c1852d2ab5f9572a09a
souichi0014 <so14@laptop.local>
2023-12-02 15:06:37 +0900

    add abc075 and modify templetes

---
+++ abc075/b/main.py
@@ -0,0 +1,4 @@
+H, W = map(int, input().split())
+S = [list(input().split()) for _ in range(H)]
+
+# 
---
+++ templete/main.py
@@ -0,0 +1,9 @@
+
+
+"""
+複数の数字の受け取り
+H, W = map(int, input().split())
+
+H行の文字列2次元配列
+S = [list(input().split()) for _ in range(H)]
+"""
