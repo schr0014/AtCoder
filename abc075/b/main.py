7be6c190caf97d3b3beee78fb38f000febfc6095
souichi0014 <so14@laptop.local>
2023-12-02 20:13:24 +0900

    ac: abc075b

--- abc075/b/main.py
+++ abc075/b/main.py
@@ -1,4 +1,34 @@
 H, W = map(int, input().split())
-S = [list(input().split()) for _ in range(H)]
+S = [list(input()) for _ in range(H)]
 
-# 
+# W列H行の2次元配列を準備する
+a = [[0 for j in range(W)] for i in range(H)]
+
+# FIXME [-1, 0, 1] を 2軸に回せば良い（[0, 0]は # ではない）
+# FIXME 右のリストでループするのも良い [[-1,-1], [0,-1], [1,-1], [-1,0], [0,0], [1,0], [-1,1], [1,-1], [1,0], [1,1]]
+
+# 2列のループを回しつつ、ターゲットの周囲を詮索せして数を足しこむ
+for h, record in enumerate(S):
+    for w, value in enumerate(record):
+        if value == "#":
+            a[h][w] = "#"
+        else:
+            if h-1 >= 0 and w-1 >= 0 and S[h-1][w-1] == "#":
+                a[h][w] += 1
+            if w-1 >= 0 and S[h][w-1] == "#":
+                a[h][w] += 1
+            if h+1 <= H-1 and w-1 >= 0 and S[h+1][w-1] == "#":
+                a[h][w] += 1
+            if h-1 >= 0 and S[h-1][w] == "#":
+                a[h][w] += 1
+            if h+1 <= H-1 and S[h+1][w] == "#":
+                a[h][w] += 1
+            if h-1 >= 0 and w+1 <= W-1 and S[h-1][w+1] == "#":
+                a[h][w] += 1
+            if w+1 <= W-1 and S[h][w+1] == "#":
+                a[h][w] += 1
+            if h+1 <= H-1 and w+1 <= W-1 and S[h+1][w+1] == "#":
+                a[h][w] += 1
+
+for record in a:
+    print ("".join(map(str, record)))
--- templete/main.py
+++ templete/main.py
@@ -1,9 +1,17 @@
 
 
 """
+## input
+
 複数の数字の受け取り
 H, W = map(int, input().split())
 
 H行の文字列2次元配列
 S = [list(input().split()) for _ in range(H)]
+
+
+## output
+
+数字と文字の混合リストを1つの文字列として出力
+print ("".join(map(str, list)))
 """
